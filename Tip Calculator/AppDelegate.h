9062701d4d6c7aa9b9f070aec3548f920eb31716
Babatope Shokunbi <babathurpe@yahoo.com>
2015-02-05 16:08:59 -0500

    Initial Commit

---
+++ Tip Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tip Calculator
+//
+//  Created by Babathurpe on 2015-02-05.
+//  Copyright (c) 2015 Babathurpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Babathurpe on 2015-02-05.
+//  Copyright (c) 2015 Babathurpe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
