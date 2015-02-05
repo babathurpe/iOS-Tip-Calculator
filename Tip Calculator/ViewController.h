f91107c46e1f2ef693a508f78fe30b91574c10ab
Babatope Shokunbi <babathurpe@yahoo.com>
2015-02-05 18:04:35 -0500

    first commit

--- Tip Calculator/ViewController.h
+++ Tip Calculator/ViewController.h
@@ -10,6 +10,18 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UITextField *receiptAmount;
+@property (strong, nonatomic) IBOutlet UITextField *partyNumber;
+@property (strong, nonatomic) IBOutlet UISlider *tipSlider;
+@property (strong, nonatomic) IBOutlet UILabel *tipSliderAmount;
+@property (strong, nonatomic) IBOutlet UILabel *tip15Amount;
+@property (strong, nonatomic) IBOutlet UILabel *customTipAmount;
+@property (strong, nonatomic) IBOutlet UILabel *totalAmount;
+@property (strong, nonatomic) IBOutlet UILabel *eachPaysAmount;
+@property (strong, nonatomic) IBOutlet UILabel *totalCustom;
+@property (strong, nonatomic) IBOutlet UILabel *partyCustom;
+
+- (IBAction)tipChanged:(UISlider *)sender;
 
 @end
 
--- Tip Calculator/ViewController.m
+++ Tip Calculator/ViewController.m
@@ -24,4 +24,43 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)tipChanged:(UISlider *)sender {
+    int tip = (int) self.tipSlider.value;
+    self.tipSliderAmount.text = [NSString stringWithFormat:@"%i", tip];
+    float nill = 0.0;
+    
+    float amount = [self.receiptAmount.text floatValue];
+    float personsNumber = [self.partyNumber.text floatValue];
+    
+    //15% tip amount
+    float percent15 = amount * 0.15;
+    self.tip15Amount.text = [NSString stringWithFormat:@"%f", percent15];
+    
+    //custom tip amount
+    float customPercent = amount * (tip/100.0);
+    self.customTipAmount.text = [NSString stringWithFormat:@"%f", customPercent];
+    
+    //total 15%
+    self.totalAmount.text = [NSString stringWithFormat:@"%f", percent15+amount];
+    
+    //total custom
+    self.totalCustom.text = [NSString stringWithFormat:@"%f", customPercent+amount];
+    
+    //each pays 15%
+    float eachParty15 = (percent15+amount)/personsNumber;
+    if (isnan(eachParty15)) {
+        self.eachPaysAmount.text = [NSString stringWithFormat:@"%f", nill];
+    } else {
+        self.eachPaysAmount.text = [NSString stringWithFormat:@"%f", eachParty15];
+    }
+    
+    //Each pays custom
+    float eachPartyCustom = (customPercent+amount)/personsNumber;
+    if (isnan(eachPartyCustom)) {
+       self.partyCustom.text = [NSString stringWithFormat:@"%f", nill];
+    } else {
+        self.partyCustom.text = [NSString stringWithFormat:@"%f", eachPartyCustom];
+    }
+}
 @end
+
